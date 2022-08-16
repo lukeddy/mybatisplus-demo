782af3ed5c13373d834e13fc382d83e7f8eb1a3e
lukeddy <tzq618766@gmail.com>
2022-08-16 16:33:02 +0800

    在日志中输出SQL语句

--- src/test/java/com/luke/mybatisplus/MyBatisTest.java
+++ src/test/java/com/luke/mybatisplus/MyBatisTest.java
@@ -5,7 +5,6 @@ import com.luke.mybatisplus.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestComponent;
 
 import java.util.List;
 
